d90f656b2c0b132499467bf2548afb5d2d3b51d8
Bence Fulop <fbence90@gmail.com>
2020-10-22 16:12:43 +1100

    Add first test and Robot class

--- spec/toy_robot_spec.rb
+++ spec/toy_robot_spec.rb
@@ -2,4 +2,9 @@ require 'spec_helper'
 
 RSpec.describe ToyRobot::Robot do
   subject { ToyRobot::Robot.new(0) }
+
+  it "moves 3 spaces" do
+    3.times { subject.move }
+    expect(subject.position).to eq(3)
+  end
 end
