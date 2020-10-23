a7f3ba94be2c0891745425618ece2b3031b407bd
Bence Fulop <fbence90@gmail.com>
2020-10-23 14:56:16 +1100

    fix style errors

--- spec/toy_robot/simulator_spec.rb
+++ spec/toy_robot/simulator_spec.rb
@@ -4,17 +4,17 @@ RSpec.describe ToyRobot::Simulator do
   let(:table) { ToyRobot::Table.new(5, 5) }
   subject { ToyRobot::Simulator.new(table) }
 
-  it "places the robot onto a valid position" do
+  it 'places the robot onto a valid position' do
     expect(ToyRobot::Robot).to receive(:new)
-      .with(0, 0, "NORTH")
+      .with(0, 0, 'NORTH')
       .and_return(double)
-    subject.place(0, 0, "NORTH")
+    subject.place(0, 0, 'NORTH')
     expect(subject.robot).not_to be_nil
   end
 
-  it "cannot place the robot onto an inposition" do
+  it 'cannot place the robot onto an inposition' do
     expect(ToyRobot::Robot).not_to receive(:new)
-    subject.place(5, 5, "NORTH")
+    subject.place(5, 5, 'NORTH')
     expect(subject.robot).to be_nil
   end
 end
