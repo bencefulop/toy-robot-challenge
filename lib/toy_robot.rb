94619c165f49a2e2bf7bae86eaeec9e7649417b2
Bence Fulop <fbence90@gmail.com>
2020-10-24 12:19:29 +1100

    Create command spec and command class to process well..commands for the robot

--- lib/toy_robot.rb
+++ lib/toy_robot.rb
@@ -2,6 +2,7 @@ require 'toy_robot/version'
 require 'toy_robot/robot'
 require 'toy_robot/table'
 require 'toy_robot/simulator'
+require 'toy_robot/command'
 
 module ToyRobot
   class Error < StandardError; end
---
+++ lib/toy_robot/command.rb
@@ -0,0 +1,5 @@
+module ToyRobot
+  class Command
+    
+  end
+end
